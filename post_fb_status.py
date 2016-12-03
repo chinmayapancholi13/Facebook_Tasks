ef063aad9baa610ecaf07cd75031f441d13d38b5
Chinmaya Pancholi <chinmayapancholi13@gmail.com>
2016-12-03 11:09:41 +0530

    updated the access token value

--- post_fb_status.py
+++ post_fb_status.py
@@ -3,9 +3,10 @@
 from facepy import GraphAPI
 
 DESIRED_STATUS = "Test status for FB"
-
 # generate this access token from 'https://developers.facebook.com/tools/explorer'
-access_token = 'EAACEdEose0cBAGkai82xt32tWaE2YoXP3YRmHrnV0C1o72a89eiP8UOTsGD3ateB8ajr15xCTGKzaDDLjd13GN3beuz3PXmWFyUHqZAxItk9gE9Eayyf7mlOWvevUOHqcoozgEysnZBUuzgiDRlwNLdcfqZAgZATXYxXYcAgHgZDZD'
+YOUR_ACCESS_TOKEN = 'abcd'
+
+access_token = YOUR_ACCESS_TOKEN
 apiConnection = GraphAPI(access_token)
 
 apiConnection.post(path='me/feed', message=DESIRED_STATUS)
